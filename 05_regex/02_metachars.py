88d0b91bed87ec342cc530a7e1bb9d9348442886
Alejandro <allaro264@gmail.com>
2026-01-30 17:50:48 +0100

    fix: :zap: regEX

--- 05_regex/02_metachars.py
+++ 05_regex/02_metachars.py
@@ -40,9 +40,45 @@ else:
     print("No se ha encontrado el patrón")
 
 
+# Cómo usar la barra invertida para anular el significado especial de un símbolo
     text = "Mi cas es banca. Y el coche es negro."
-    pattern = "."
+    pattern = r"\."
 
     matches = re.findall(pattern, text)
 
     print(matches)
+
+
+# \d: coincide con cualquier dígito (0-9)
+
+text = "El nuero de telefono es 123456789"
+found = re.findall(r'\d{9}', text)
+
+print(found)
+
+#Ejercicio: Detectar si hay un número de España en el text gracias al prefijo +34
+
+text = "Mi número de teléfono es +34 6889999999 apúntado?"
+pattern = r"\+34 \d{9}"
+found = re.search(pattern, text)
+if found: print(f"Encontré el número de teléfono {found.group()}")
+
+# \w: Coincide con cualquier caracter alfanumerico (a-z, A-Z, 0-9, _)
+text = "el_marcos_21)/&"
+pattern = r"\w"
+found = re.findall(pattern, text)
+print(found)
+
+
+# \s: Coincide con cualquier espacio en blanco (espacio, tabulaciñon, salto de línea )
+text = "Hola mundo\n¿Cómo estás\t"
+pattern = r"\s"
+matches = re.findall(pattern, text)
+print(matches)
+
+# ^: Coincide con el principio de una cadena
+text = "@423_name"
+valid = r"^\w" #validar nombre de usuario
+
+if valid: print("El nombre de usuario es válido")
+else: print("El nombre de usuario no es válido")
