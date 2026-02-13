5b12006f66bc563b98a43a1154185c4b4d223ca6
Alejandro <allaro264@gmail.com>
2026-02-13 17:51:03 +0100

    fix: :zap: WIP

--- 06_request_ai_dates/02_request.py
+++ 06_request_ai_dates/02_request.py
@@ -5,17 +5,17 @@
 
 import urllib.request
 import json
-api_posts = "https://jsonplaceholder.typicode.com/posts/"
+# api_posts = "https://jsonplaceholder.typicode.com/posts/"
 
 
-try:
-    response = urllib.request.urlopen(api_posts)
-    data = response.read()
-    json_data = json.loads(data.decode('utf-8'))
-    print(json_data)
-    response.close()
-except urllib.error.URLError as e:
-    print(f"Error en la solicitud: {e}")
+# try:
+#     response = urllib.request.urlopen(api_posts)
+#     data = response.read()
+#     json_data = json.loads(data.decode('utf-8'))
+#     print(json_data)
+#     response.close()
+# except urllib.error.URLError as e:
+#     print(f"Error en la solicitud: {e}")
 
 
 # 2. Con dependencias (request)
@@ -24,5 +24,5 @@ import requests
 print("\nGET:")
 api_posts = "https://jsonplaceholder.typicode.com/posts/"
 response = requests.get(api_posts)
-json = response.josn()
+json = response.json()
 print(json[0])
