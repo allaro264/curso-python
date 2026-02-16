3b520c78e257f86f8538a82f531eec23bcc455f1
Alejandro <allaro264@gmail.com>
2026-02-16 13:56:55 +0100

    feat: :zap: Python request

--- 06_request_ai_dates/02_request.py
+++ 06_request_ai_dates/02_request.py
@@ -5,6 +5,9 @@
 
 import urllib.request
 import json
+
+# DEEPSEEK_API_KEY = "xxx"
+
 # api_posts = "https://jsonplaceholder.typicode.com/posts/"
 
 
@@ -24,5 +27,85 @@ import requests
 print("\nGET:")
 api_posts = "https://jsonplaceholder.typicode.com/posts/"
 response = requests.get(api_posts)
-json = response.json()
+response_json = response.json()
 print(json[0])
+
+
+# 3. Un Post
+print("\nPOST:")
+try:
+    response = requests.post("https://jsonplaceholder.typicode.com/posts/", 
+    json={
+        "title": "midu",
+        "body": "dev",
+        "userId": 5
+    })
+    print(response.status_code)
+
+except requests.excetions.RequestException as e:
+    print(f"Error en la solicitud: {e}")
+
+
+# 4. Un PUT 
+print("\nPUT:")
+try:
+    response = requests.put("https://jsonplaceholder.typicode.com/posts/1", 
+    json={
+        "title": "midu",
+        "body": "dev",
+        "userId": 5
+    })
+    print(response.status_code)
+
+except requests.excetions.RequestException as e:
+    print(f"Error en la solicitud: {e}")
+
+    # User la API de GPT-4o de OpenAI
+
+#     OPENAI_KE="sk-XXXXXXXX"
+
+#     import json
+
+# def call_openai_gpt(api_key, prompt):
+#     url = "https://api.openai.com/v1/chat/completions"
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"bearer {api_key}"
+#     }
+#     data = {
+#         "model": "gpt-4o-mini",
+#         "messages": [{"role": "user", "content": prompt}]
+#     }
+
+#     response = requests.post(url, json=data, headers=headers)
+#     return response.json()
+
+# api_response = call_openai_gpt(OPENAI_KEY, "Esctibe un breve poema sobre la programación")
+
+# print(json.dumps(api_response, indent=2))
+
+# print(api_response["choises"][0]["message"]["content"])
+
+# Llamar a la api de deep sheek
+
+# import json
+
+# def call_deepseek(api_key, prompt):
+#     url = "https://api.deepseek.com/chat/completions"
+#     headers = {
+#         "Content-Type": "application/json",
+#         "Authorization": f"bearer {api_key}"
+#     }
+#     data = {
+#         "model": "deepseek-chat",
+#         "messages": [{"role": "user", "content": prompt}]
+#     }
+
+#     response = requests.post(url, json=data, headers=headers)
+#     return response.json()
+
+# api_response = call_deepseek(DEEPSEEK_API_KEY, "Esctibe un breve poema sobre la programación")
+
+# print(json.dumps(api_response, indent=2))
+
+# print(api_response["choises"][0]["message"]["content"])
