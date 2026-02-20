037e1a08ded13ea40eb71c27b81d37f8b86b47a3
Alejandro <allaro264@gmail.com>
2026-02-20 18:30:34 +0100

    feat: :rocket: Fin del Curso de Python

---
+++ 07_scraping/06_playwright_scraping.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+url = 'https://midu.dev'
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=2000, ) #utilizar un navegador
+    page = browser.new_page()
+    page.goto(url)
+
+    first_article_anchor = page.locator('article a').first
+    first_article_anchor.click()
+
+    page.wait_for_load_state()
+
+    curso_content_container = page.locator('text="Contenido del curso"')
+    curso_content_sibling = curso_content_container.locator('xpath=./div/')
+
+    browser.close()
+
+
